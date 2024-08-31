1dfd34aa264e46fc720fb02d0d67e4d5e057b556
ValentinoL15 <pichilongo1@gmail.com>
2024-08-31 15:55:05 -0300

    update

--- ionic/src/app/token.interceptor.ts
+++ ionic/src/app/token.interceptor.ts
@@ -8,21 +8,25 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
+import { SpinnerService } from './services/spinner.service';
 
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private readonly spinnerService: SpinnerService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
+
+    this.spinnerService.show();
     const authToken = localStorage.getItem("st_1892@121");
 
     const authRequest = request.clone({
       headers: request.headers.set('Authorization', `Bearer ${authToken}`)
     });
 
-    return next.handle(authRequest)
+    return next.handle(authRequest).pipe(
+      finalize(() => this.spinnerService.hide())
+    )
   }
 }
