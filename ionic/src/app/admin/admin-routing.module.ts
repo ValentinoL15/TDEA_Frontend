23fda1613b9d46f19e8ff50768b51aeb46901dfc
ValentinoL15 <pichilongo1@gmail.com>
2024-10-02 17:39:20 -0300

    update

--- ionic/src/app/admin/admin-routing.module.ts
+++ ionic/src/app/admin/admin-routing.module.ts
@@ -112,7 +112,15 @@ const routes: Routes = [
   {
     path: 'informes-info/:id',
     loadChildren: () => import('./informes-info/informes-info.module').then( m => m.InformesInfoPageModule)
+  },  {
+    path: 'create-admin',
+    loadChildren: () => import('./create-admin/create-admin.module').then( m => m.CreateAdminPageModule)
+  },
+  {
+    path: 'my-users',
+    loadChildren: () => import('./my-users/my-users.module').then( m => m.MyUsersPageModule)
   }
+
 
 ];
 
---
+++ ionic/src/app/admin/create-admin/create-admin.page.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { parsePhoneNumber } from 'libphonenumber-js';
+import { NotifyService } from 'src/app/services/notify.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-create-admin',
+  templateUrl: './create-admin.page.html',
+  styleUrls: ['./create-admin.page.scss'],
+})
+export class CreateAdminPage implements OnInit {
+form: FormGroup
+constructor(private fb: FormBuilder, private notifyService: NotifyService, private router: Router, private userService: UserService) {
+  this.form = this.fb.group({
+    docNumber: ['', [Validators.required]],
+    firstName: ['', [Validators.required, Validators.minLength(3)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)]],
+    gender:['', [Validators.required]],
+    email: ['', [Validators.required], [this.validateEmailAsync] ],
+    password: ['', [Validators.required]],
+    phone: ['+54', [Validators.required]],
+    rol: ['', Validators.required]
+  })
+}
+
+validateEmailAsync: AsyncValidatorFn = (control: AbstractControl): Promise<ValidationErrors | null> => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const valid = emailRegex.test(control.value);
+  return new Promise(resolve => {
+    setTimeout(() => { // Simular una solicitud asíncrona (puedes omitir esto si tu validación no requiere un retraso)
+      resolve(valid ? null : { invalidEmail: true });
+    }, 1000); // Tiempo de espera simulado de 1 segundo
+  });
+};
+
+ngOnInit() {
+}
+
+volver(){
+  this.router.navigate(['/my-users'])
+}
+
+createAdmin(){
+  if(this.form.valid){
+    const phoneNumberValue = this.form.value.phone;
+
+    let phoneNumber;
+    try {
+      phoneNumber = parsePhoneNumber(phoneNumberValue, 'AR'); // 'AR' es el código de país para Argentina
+      
+      if (phoneNumber && phoneNumber.isValid()) {
+        console.log('Número de teléfono válido:', phoneNumber.number); // Número formateado
+      } else {
+        console.error('Número de teléfono inválido');
+        this.notifyService.error('Número de teléfono inválido, el número debe ser con formato: +54-1111-123456');
+        return; // Salir de la función si el número no es válido
+      }
+    } catch (error) {
+      console.error('Error al parsear el número de teléfono:', error);
+      this.notifyService.error('Error al validar el número de teléfono');
+      return; // Salir de la función si ocurre un error
+    }
+
+    const formulario = {
+      firstName: this.form.value.firstName,
+      lastName: this.form.value.lastName,
+      docNumber: this.form.value.docNumber,
+      email: this.form.value.email,
+      password: this.form.value.password,
+      rol: this.form.value.rol,
+      gender: this.form.value.gender,
+      phone: this.form.value.phone
+    }
+    this.userService.createAdmin(formulario).subscribe({
+      next: (res : any) => {
+        this.notifyService.success(res.message)
+        this.form.reset()
+      },
+      error: (err: any) => {
+        this.notifyService.error(err.error.message)
+      }
+    })
+  }
+}
+
+onPhoneInputChange(event: any) {
+  let inputValue = event.detail.value;
+
+  // Filtra solo los números después del prefijo +54
+  const numericValue = inputValue.replace(/^\+54\s*/, '').replace(/\D/g, '');
+
+  // Mantén el formato deseado, como +54 2477
+  let formattedValue = '+54';
+  if (numericValue.length > 0) {
+    formattedValue += '' + numericValue;
+  }
+
+  // Actualiza el valor del campo de teléfono
+  const phoneControl = this.form.get('phone');
+  phoneControl?.setValue(formattedValue, { emitEvent: false });
+}
+
+
+
+}
