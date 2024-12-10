b1579d2a4438ef63381986fa4268a56c82bef91a
ValentinoL15 <pichilongo1@gmail.com>
2024-12-10 18:17:12 -0300

    up

--- ionic/src/app/admin-Jugador/home-jugador/home-jugador.page.ts
+++ ionic/src/app/admin-Jugador/home-jugador/home-jugador.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-jugador',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeJugadorPage implements OnInit {
 
-  constructor() { }
+constructor(private router:Router) { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+}
+
+logOut() {
+  localStorage.removeItem('st_1892@121');  // Elimina el token del localStorage
+  this.router.navigate(['/login'] , { replaceUrl: true });     
+}
 
 }
