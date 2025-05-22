0337d82e74dda23fbb6eff0976c918e95eb632a1
ValentinoL15 <pichilongo1@gmail.com>
2025-05-22 10:14:44 -0300

    update tables in screen users

--- ionic/src/app/inscripciones/inscripciones.page.ts
+++ ionic/src/app/inscripciones/inscripciones.page.ts
@@ -4,6 +4,8 @@ import { NotifyService } from '../services/notify.service';
 import { UserService } from '../services/user.service';
 import { TournamentService } from '../services/tournament.service';
 import { List } from '../interfaces/List';
+import { Player } from '../interfaces/Player';
+import { Tournament } from '../interfaces/Tournament';
 
 @Component({
   selector: 'app-inscripciones',
@@ -116,17 +118,119 @@ list: List = {
   }]
   }
 }
-tournament: any
+tournament: Tournament = {
+  nameFantasy: "",
+  ano: 0,
+  order: 0,
+  campeonato:{
+    _id: "",
+    type: ""
+  },
+  edad: {
+    _id: "",
+    type: ""
+  },
+  daysTournament: [{
+    _id: "",
+    day: {
+      _id: "",
+      type: ""
+    },
+    sede: {
+      images: []
+    },
+    stadium: {
+      _id: "",
+      belongToSede: "",
+      code: "",
+      type: "",
+      length: 0,
+      width: 0,
+      roof: "",
+      grass: "",
+      punctuaction: "",
+    },
+    time: []
+  }],
+  teamSubscribed: [{
+    _id: "",
+    preferences: []
+  }],
+  rangeAgeSince: 0,
+  rangeAgeUntil: 0,
+  ageDescripcion: "",
+  category: {
+    _id: "",
+    categoryName : "",
+  },
+  format: {
+    _id:"",
+  formatName: "",
+  minPlayers: 0,
+  maxPlayers: 0
+  },
+  tournamentDate: new Date(),
+  tournamentNotes: "",
+  isTournamentMasculine: false,
+  isTournamentActive: false,
+  tarifaInscripcion: 0,
+  tarifaPartido: 0,
+  deposito:0,
+  cupos: 0,
+  fixture: [{
+    _id: "",
+    jornada: 0,
+    partidos: [{
+      team1: {
+        _id: '',
+        nameList: '', 
+    },
+    team2: {
+        _id: '',
+        nameList: '',
+    },
+    resultado: {
+        
+            team1: 0,
+            team2: 0
+        
+    }
+    }]
+  }],
+  tablaPosiciones: [{
+        team: {
+          _id: '',
+          nameList: '',
+          typeAlineacion: 0,
+          teamPicture: ''
+        },
+        puntos: 0,
+        partidosJugados: 0,
+        ganados: 0,
+        empatados: 0,
+        perdidos: 0,
+        golesAFavor: 0,
+        golesEnContra: 0,
+        diferenciaGoles: 0
+  }]
+}
+goleador: Player[] = [];
+valla: any[] = [];
+fairPlay: any[] = [];
 
 ngOnInit() {
   this.getTournamentList()
+
 }
 
 getTournamentList(){
   this.userService.getTournamnetsList().subscribe({
     next: (res : any) => {
       this.list.tournamentRegistration = res.tournament
-      console.log(this.list) 
+      console.log(this.list.tournamentRegistration) 
+      this.goleadores()
+      this.vallaMenosVencida()
+      this.getFairPLay()
     },
     error: (err) => {
       this.notifyService.error(err.error.message)
@@ -138,8 +242,8 @@ getTournament(){
   this.tournamentServ.getTournament(this.list.tournamentRegistration?._id).subscribe({
     next: (res : any) => {
       this.tournament = res.tournamentFound
-      console.log(this.tournament)
-      console.log("Fixture cargado:", JSON.stringify(this.tournament.fixture, null, 2));
+      console.log("torneo:",this.tournament)
+      
       this.tournament.fixture.forEach((jornada : any) => {
         jornada.partidos.forEach((match : any) => {
           if (!match.resultado) {
@@ -150,6 +254,40 @@ getTournament(){
           }
         });
       });
+    },
+    error: (err : any) => {
+      this.notifyService.error(err.error.message)
+    }
+  })
+}
+
+goleadores(){
+  this.tournamentServ.getGoleador(this.list.tournamentRegistration?._id).subscribe({
+    next: (res : any) => {
+      this.goleador = res.orederedGoleadores
+      console.log(this.goleador)
+    },
+    error: (err : any) => {
+      this.notifyService.error(err.error.message)
+    }
+  })
+}
+
+vallaMenosVencida(){
+  this.tournamentServ.getVallaMenosVencida(this.list.tournamentRegistration?._id).subscribe({
+    next: (res : any) => {
+      this.valla = res.equipos;
+    },
+    error: (err : any) => {
+      this.notifyService.error(err.error.message)
+    }
+  })
+}
+
+getFairPLay(){
+  this.tournamentServ.getFairPLay(this.list.tournamentRegistration?._id).subscribe({
+    next: (res : any) => {
+      this.fairPlay = res.fairPlayData
     },
     error: (err : any) => {
       this.notifyService.error(err.error.message)
